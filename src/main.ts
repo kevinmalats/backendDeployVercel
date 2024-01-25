8596d3437768e6472330a6c66b098a45b4d0b681
Kevin Malats <desarrollo.externokmq@crecoscorp.com>
2024-01-25 15:34:13 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ let port: number = parseInt(process.env.PORT) || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,PUT,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   try {
     await app.listen(port);
     console.log(`Server running on port ${port}`);
